fc63e70b7e7982dfe4b50620b495c8e2a16af18d
jsmit496 <jsmit496@stumail.jccc.edu>
2018-02-15 13:36:38 -0600

    fixed else problem

--- update.bat
+++ update.bat
@@ -6,7 +6,12 @@ set /P yesOrNo=Are you sure you want to do this(Y/N)?
 IF %yesOrNo%==Y	OR IF %yesOrNo%==y(
 	call git fetch --all
 	call git merge -X theirs
+	GOTO:ending	
+) ELSE 
+	echo update cancelled. If you have any conflicting files be sure to fix them.
 )
+
+:ending
 endlocal
 
 
