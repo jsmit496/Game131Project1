2feb85db9e3f39225e8aee58cd55bf66796232e6
svc-student <svc-student@student.directory.jccc>
2018-02-15 18:38:27 -0600

    hi

--- commit.bat
+++ commit.bat
@@ -10,7 +10,7 @@ setlocal
 set /P yesOrNo=Would you like to try and push (Y/N)?
 IF %yesOrNo%==Y OR IF %yesOrNo%==y	(
 	call git push
-	GOTO:ending
+	GOTO ending
 ) ELSE IF (
 	echo changes were left commited but not pushed.
 )
